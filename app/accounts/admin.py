46c422941f2856cffaaf438cc5ba2783daabb9a1
Nick <nick.smith.tsa@gmail.com>
2021-06-20 15:24:42 +0100

    sub and account classes

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from accounts import models
 
-admin.site.register(Account, UserAdmin)
+# does nothing
+
+
+class SubscriberInline(admin.TabularInline):
+    model = models.Subscriber
+    extra = 1
+
+
+admin.site.register(models.Account, UserAdmin)
+
+# does noting
+
+
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [SubscriberInline, ]
+    #inlines = (SubscriberInline, )
+
+
+@admin.register(models.Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "email",
+                    "date_joined", "verified", "account")
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -4,14 +4,27 @@ from django.contrib.auth.models import AbstractUser
 
 class Account(AbstractUser):
     pass
-    # ensure has fields: subs, em_messages, automations, landing_pages, first_name, last_name, email, date_joined, password
+    # subscriber = ____ < one:many
+    # subscribers = models.ForeignKey(
+    # Subscriber, null = True, on_delete = models.SET_NULL)
+    # need to add fields: subs, em_messages, automations, landing_pages
+    # may need to define some fields as classes in separate apps, then add as foreign key
+    # need to make password field required
     # should also use email field instead of username to log in, and require email instead of username when creating accounts
     # set USERNAME_FIELD & REQUIRED_FIELDS?
 
 
 class Subscriber(models.Model):
-    first_name = models.CharField(max_length=100, null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.CharField(max_length=100, null=True)
-    # tags = _________________
+    # tags = _________________ many to many field, with later option of users being able to define custom tags
     date_joined = models.DateTimeField(auto_now_add=True, null=True)
     verified = models.BooleanField(default=False)
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email
+
+    list_display = ('first_name', 'email', 'date_joined', 'verified')
