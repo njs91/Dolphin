03fa031c796955be52f02c340550f1866f172244
Nick <nick.smith.tsa@gmail.com>
2021-06-21 22:31:45 +0100

    dynamic account page

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/', views.account),
-    path('account/edit/', views.edit_account),
+    path('account/<str:id>/', views.account, name="account"),
+    path('account/edit/', views.edit_account, name="edit_account"),
 ]
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from accounts.models import Account
+from subscribers.models import Subscriber
 from .models import *
 
 
-def account(request):
-    return render(request, 'accounts/account.html')
+def account(request, id):
+    account = Account.objects.get(id=id)
+    # subscribers = account.subscribers_set.all()
+    context = {'account': account}
+    return render(request, 'accounts/account.html', context)
 
 
 def edit_account(request):
