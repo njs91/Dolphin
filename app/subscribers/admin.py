ad0e57152c61cd64f60075fcbdd0bbdb5709e5f4
Nick <nick.smith.tsa@gmail.com>
2021-06-24 23:10:14 +0100

    tag model many:many with subscribers

--- app/subscribers/admin.py
+++ app/subscribers/admin.py
@@ -3,7 +3,24 @@ from django.contrib.auth.admin import UserAdmin
 from subscribers import models
 
 
+# class TagInline(admin.TabularInline):
+#     model = models.Tag.subscribers.through
+#     extra = 0
+# unneeded - individual tag pages already show their subscribers
+
+
+# class SubscriberInline(admin.TabularInline):
+#     model = models.Subscriber.tags.through # ERROR: attributeError: type object 'Subscriber' has no attribute 'tags'
+
+
 @admin.register(models.Subscriber)
 class SubscriberAdmin(admin.ModelAdmin):
     list_display = ("first_name", "email",
                     "date_joined", "verified", "account")
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = models.Subscriber
+    # inlines = [TagInline, ] # inlines unneeded - individual tag pages already show their subscribers
+    list_display = ("name", )
--- app/subscribers/models.py
+++ app/subscribers/models.py
@@ -6,7 +6,8 @@ class Subscriber(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.CharField(max_length=100, null=True)
-    # tags = _________________ many to many field, with later option of users being able to define custom tags
+    # tags: many to many field, with later option of users being able to define custom tags
+    # tags = models.ManyToManyField(Tag, blank=true) # cannot uncomment, otherwise error: "NameError: name 'Tag' is not defined" - but already has many:many relationship - just not showing in django admin
     date_joined = models.DateTimeField(auto_now_add=True, null=True)
     verified = models.BooleanField(default=False)
     account = models.ForeignKey(
@@ -16,3 +17,11 @@ class Subscriber(models.Model):
         return self.email
 
     list_display = ('first_name', 'email', 'date_joined', 'verified')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    subscribers = models.ManyToManyField(Subscriber, blank=True)
+
+    def __str__(self):
+        return self.name
