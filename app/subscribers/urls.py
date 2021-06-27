ed568b7872aeb5b04d86b9515ee6a6f5db9e6184
Nick <nick.smith.tsa@gmail.com>
2021-06-27 14:32:09 +0100

    individual subscriber view

--- app/subscribers/urls.py
+++ app/subscribers/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('account/<str:pk>/subscribers/',
          views.get_subscribers, name="get_subscribers"),
+    path('account/<str:pk>/subscribers/<str:sub_id>',
+         views.get_subscriber, name="get_subscriber"),
 ]
--- app/subscribers/views.py
+++ app/subscribers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from accounts.models import Account
+from subscribers.models import Subscriber
 
 
 def get_subscribers(request, pk):
@@ -7,3 +8,10 @@ def get_subscribers(request, pk):
     subscribers = account.subscriber_set.all()
     context = {'account': account, 'subscribers': subscribers}
     return render(request, 'subscribers/subscribers.html', context)
+
+
+def get_subscriber(request, pk, sub_id):
+    account = Account.objects.get(id=pk)  # pk needed? it'll be in the URL
+    subscriber = Subscriber.objects.get(id=sub_id)
+    context = {'account': account, 'subscriber': subscriber}
+    return render(request, 'subscribers/subscriber.html', context)
