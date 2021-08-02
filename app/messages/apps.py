07cc66f47f3bbed41bf186a8a77f08207da6ed76
Nick <nick.smith.tsa@gmail.com>
2021-08-02 20:36:44 +0100

    messages migrations etc

--- app/messages/apps.py
+++ app/messages/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class MessagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messages'
-    # label = 'email.messages'
-    # default_app_config = 'messages.apps.MessagesConfig'
+    # label required, otherwise duplicate naming issue with INSTALLED_APPS
+    label = 'email_messages'
--- app/messages/models.py
+++ app/messages/models.py
@@ -3,10 +3,10 @@ from accounts.models import Account
 
 
 class Message(models.Model):
-    name = models.CharField(max_length=100),
-    subject = models.CharField(max_length=128),
-    text = models.TextField(),
-    account = models.ForeignKey(Account, on_delete=models.CASCADE),
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=128)
+    text = models.TextField()
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
     #automations = models.ManyToMainField(Automation)
 
     def __str__(self):
