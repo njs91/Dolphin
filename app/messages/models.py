94bf6a679e218e7c1bb9539933a10862b6dee2c9
Nick <nick.smith.tsa@gmail.com>
2021-08-07 00:42:44 +0100

    circular import bug fix

--- app/messages/models.py
+++ app/messages/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 from accounts.models import Account
-# from automations.models import Automation  # < FAILS
-from automations.models import *  # < WORKS
-# from apps.get_model(email_messages, Message)   # < FAILS
-# from django.apps get_model(email_messages, Message)  # < FAILS
-# from django.apps import apps  # < FAILS
-# Automations = apps.get_model('email_messages', 'Messages')  # < FAILS
 
 
 class Message(models.Model):
@@ -14,7 +8,7 @@ class Message(models.Model):
     text = models.TextField()
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    automations = models.ManyToManyField(Automation)
+    automations = models.ManyToManyField('automations.Automation')
 
     def __str__(self):
         return self.name
