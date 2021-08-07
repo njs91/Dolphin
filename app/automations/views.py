c55e46dbfda761d48fc246bb209506c37ba8f249
Nick <nick.smith.tsa@gmail.com>
2021-08-07 15:07:20 +0100

    list associated messages on automation pages

--- app/automations/views.py
+++ app/automations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from accounts.models import Account
 from automations.models import Automation
+from messages.models import Message
 from .forms import AutomationForm
 from .filters import AutomationFilter
 from django.contrib.auth.decorators import login_required
@@ -48,7 +49,9 @@ def automation_create(request, pk):
 def automation_view(request, pk, automation_id):
     account = Account.objects.get(id=pk)  # pk needed? it'll be in the URL
     automation = Automation.objects.get(id=automation_id)
-    context = {'account': account, 'automation': automation}
+    messages = Message.objects.filter(automations__account=account)
+    context = {'account': account,
+               'automation': automation, 'messages': messages}
     return render(request, 'automations/automation_view.html', context)
 
 
